f23fc2f0446ca2561e9d1e04d9986a42614666ca
thaiyue <thaiyu@gmail.com>
2018-11-16 14:10:18 +0100

    FlipCard shit

--- frontend/src/components/HeroCard/HeroCard.js
+++ frontend/src/components/HeroCard/HeroCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./HeroCard.scss"
 import Speech from 'react-speech'
+import FlipCard from 'react-flipcard'
 
 class HeroCard extends Component {
 
@@ -18,23 +19,73 @@ class HeroCard extends Component {
       )
     }
 
+    getInitialState() {
+    return {
+      isFlipped: false
+    };
+  }
+
+  showBack() {
+    this.setState({
+      isFlipped: true
+    });
+  }
+
+  showFront() {
+    this.setState({
+      isFlipped: false
+    });
+  }
+
+  handleOnFlip(flipped) {
+    if (flipped) {
+      this.refs.backButton.getDOMNode().focus();
+    }
+  }
+
+  handleKeyDown(e) {
+    if (this.state.isFlipped && e.keyCode === 27) {
+      this.showFront();
+    }
+  }
+
 
   render() {
      {
 
 
     return (
+      <FlipCard
+        disabled={true}
+        flipped={this.state.isFlipped}
+        onFlip={this.handleOnFlip}
+        onKeyDown={this.handleKeyDown}
+      >
 
-      <div className ="singleLetterComponent">
-        <div className="characterImage">
-          <img src={this.state.letter.characters[0].image} />
-          <Speech
-            text={`${this.state.letter.letterChar} is for ${this.state.letter.characters[0].name}.`}
-            textAsButton={true}
-            rate=".95"
-          voice="Google UK English Female" />
+        <div className ="frontCard">
+          <div className="characterImage">
+            <img src={this.state.letter.characters[0].image} />
+            <Speech
+              text={`${this.state.letter.letterChar} is for ${this.state.letter.characters[0].name}.`}
+              textAsButton={true}
+              rate=".95"
+            voice="Google UK English Female" />
+            <button type="button" onClick={this.showBack}>Show back</button>
+          </div>
         </div>
-      </div>
+
+        <div className ="backCard">
+          <div className="characterImage">
+            <img src={this.state.letter.characters[0].image} />
+            <Speech
+              text={`${this.state.letter.letterChar} is for ${this.state.letter.characters[0].name}.`}
+              textAsButton={true}
+              rate=".95"
+            voice="Google UK English Female" />
+            <button type="button" ref="backButton" onClick={this.showFront}>Show front</button>          </div>
+        </div>
+
+      </FlipCard>
     )
   }
 }
