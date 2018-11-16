9b3a1f8a7a0accaf1af895953cbc20c31f40f580
thaiyue <thaiyu@gmail.com>
2018-11-16 12:08:28 +0100

    Flippy

--- frontend/src/components/HeroCard/HeroCard.js
+++ frontend/src/components/HeroCard/HeroCard.js
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
 import "./HeroCard.scss"
 import Speech from 'react-speech'
+import Flippy, { FrontSide, BackSide } from 'react-flippy'
 
 class HeroCard extends Component {
 
   constructor(props) {
     super(props)
      this.state = {
-       ...props,
-
+       ...props
       }
-    ;
   }
-
 
 
   componentWillReceiveProps(props){
@@ -27,20 +25,42 @@ class HeroCard extends Component {
 
 
     return (
-        <div className ="Letter_Front" key="front">
-          <div className="characterImage">
-            <img src={this.state.letter.characters[0].image} />
-            <Speech
-              text={`${this.state.letter.letterChar} is for ${this.state.letter.characters[0].name}.`}
-              textAsButton={true}
-              rate=".95"
-            voice="Google UK English Female" />
+
+      <Flippy
+        flipOnHover={false}
+        flipOnClick={true}
+        flipDirection="horizontal"
+      ref={(r) => this.flippy = r}>
+
+        <FrontSide>
+          <div className ="LetterFront">
+            <div className="characterImage">
+              <img src={this.state.letter.characters[0].image} />
+              <Speech
+                text={`${this.state.letter.letterChar} is for ${this.state.letter.characters[0].name}.`}
+                textAsButton={true}
+                rate=".95"
+              voice="Google UK English Female" />
+            </div>
           </div>
-        </div>
+        </FrontSide>
 
+        <BackSide>
+          <div className ="LetterBack">
+            <div className="characterImage">
+              <img src={this.state.letter.characters[0].image} />
+              <Speech
+                text={`${this.state.letter.letterChar} is for ${this.state.letter.characters[0].name}.`}
+                textAsButton={true}
+                rate=".95"
+              voice="Google UK English Female" />
+            </div>
+          </div>
+        </BackSide>
+      </Flippy>
+        )
+        }
+        }
+        }
 
-    )
-  }
-}
-}
-export default HeroCard;
+        export default HeroCard;
